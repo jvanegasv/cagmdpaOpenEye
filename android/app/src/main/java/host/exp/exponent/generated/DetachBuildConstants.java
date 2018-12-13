3d277eb887a6cf3fbfc4ac4f8acedcd4f4ed2843
Jorge Vanegas <jvanegasv@gmail.com>
2018-12-13 09:58:09 -0600

    cagmdpaOpenEye empty

---
+++ android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
@@ -0,0 +1,8 @@
+package host.exp.exponent.generated;
+
+// This file is auto-generated. Please don't rename!
+public class DetachBuildConstants {
+
+  public static final String DEVELOPMENT_URL = "expd3d2b7ea7ec24736873ca35951686596://192.168.0.199:19000";
+
+}
